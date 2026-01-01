de1e163426627b2b79d8e29e6833a55b34fa2ef5
kyj4528 <kyj4528@naver.com>
2026-01-01 17:30:02 +0900

    Initial commit

---
+++ src/main/java/com/aenggukland/letspt/LetSptApplication.java
@@ -0,0 +1,13 @@
+package com.aenggukland.letspt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetSptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetSptApplication.class, args);
+    }
+
+}
